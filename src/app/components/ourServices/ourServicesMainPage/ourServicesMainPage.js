bdbbbc4a61f326de5f6ffd04f9a998e9f180bd59
Snipbyte <snipbytesol@gmail.com>
2025-08-23 20:08:09 +0500

    eliya

--- src/app/components/ourServices/ourServicesMainPage/ourServicesMainPage.js
+++ src/app/components/ourServices/ourServicesMainPage/ourServicesMainPage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Footer from '../../common/footer/footer'
-import HeroSection from '../heroSection/heroSection'
 import SolutionSection from '../solutionSection/solutionSection'
 import FeaturedSection from '../featuredSection/featuredSection'
 import TeamSection from '../teamSection/teamSection'
 import Header from '../../common/header/header'
+import dynamic from 'next/dynamic';
+import Header from '../../common/header/header'
+const HeroSection = dynamic(() => import('../heroSection/heroSection'), {
+  ssr: false,
+});
 
 const OurServicesMainPage = () => {
     return (
