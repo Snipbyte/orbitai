b43b8f5ece269ad828a7a05447aac4991b65b534
Snipbyte <snipbytesol@gmail.com>
2025-09-29 15:51:14 +0500

    aiorbit

--- src/app/components/ourServices/ourServicesMainPage/ourServicesMainPage.js
+++ src/app/components/ourServices/ourServicesMainPage/ourServicesMainPage.js
@@ -17,7 +17,6 @@ const OurServicesMainPage = () => {
             <HeroSection/>
             <SolutionSection/>
             <FeaturedSection/>
-            
             {/* <TeamSection/> */}
             <Footer/>
         </div>
